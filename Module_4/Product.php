e3b83da11954f963f7a87612e8f8f112e7a3ad1c
fardin135 <142721195+fardin135@users.noreply.github.com>
2023-10-04 22:52:19 +0600

    Add files via upload

---
+++ Module_4/Product.php
@@ -0,0 +1,28 @@
+<?php
+class Product
+{
+    public $id, $name, $price;
+
+    function __construct($id, $name, $price)
+    {
+        $this->id    = $id;
+        $this->name  = $name;
+        $this->price = $price;
+    }
+
+    function getFormattedPrice()
+    {
+        return "- Price: $" . number_format($this->price, 2);
+    }
+
+    function showDetails()
+    {
+        echo "Product Details:<br>";
+        echo "- ID: {$this->id}<br>";
+        echo "- Name: {$this->name}<br>";
+        echo $this->getFormattedPrice();
+    }
+}
+
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
